e0c55af5987e4ed92d53d0fe9fc7a08dcf5fadef
Zeeraa <udenanton@hotmail.com>
2018-05-14 13:40:56 +0200

    Add project files

--- ScreenInfo/Program.cs
+++ ScreenInfo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScreenInfo
 {
@@ -10,6 +11,16 @@ namespace ScreenInfo
     {
         static void Main(string[] args)
         {
+            foreach(Screen screen in Screen.AllScreens)
+            {
+                decimal mpixel = (decimal)(screen.Bounds.Width * screen.Bounds.Height) / 1000000;
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Device name: " + screen.DeviceName);
+                Console.WriteLine("Is primary: " + screen.Primary);
+                Console.WriteLine("Resolution: " + screen.Bounds.Width + "x" + screen.Bounds.Height + " " + mpixel + " Megapixels");
+                Console.WriteLine("Bits per pixel: " + screen.BitsPerPixel);
+            }
+            Console.ReadKey();
         }
     }
 }
